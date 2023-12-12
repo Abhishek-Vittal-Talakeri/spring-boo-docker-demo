2c4e7b0b4cf9f933660c78a4a4dd799983a09742
Abhishek <abhishek.talakeriv@gmail.com>
2023-12-12 20:04:55 +0530

    First commit

---
+++ spring-boot-docker-demo/src/main/java/com/example/demo/SpringBootJarApplication.java
@@ -0,0 +1,36 @@
+package com.example.demo;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+@RestController
+public class SpringBootJarApplication {
+
+	public static Logger log = LoggerFactory.getLogger(SpringBootJarApplication.class);
+
+	@PostConstruct
+	public void init() {
+		log.info("Application is strarted");
+	}
+
+	public static void main(String[] args) {
+
+		log.info("Application is executed");
+
+		SpringApplication.run(SpringBootJarApplication.class, args);
+	}
+
+	@GetMapping("/test")
+	public String test() {
+
+		return "Test Messges";
+	}
+
+}

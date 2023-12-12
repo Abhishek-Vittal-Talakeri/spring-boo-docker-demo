96678d1f1958d1837d5e97579fb28f4ffa6e6097
Abhishek <abhishek.talakeriv@gmail.com>
2023-12-12 20:10:00 +0530

    test first

--- spring-boot-docker-demo/src/test/java/com/example/demo/SpringBootJarApplicationTests.java
+++ spring-boot-docker-demo/src/test/java/com/example/demo/SpringBootJarApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
+import static org.junit.Assert.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 class SpringBootJarApplicationTests {
+
+	public static Logger log = LoggerFactory.getLogger(SpringBootJarApplicationTests.class);
 
 	@Test
 	void contextLoads() {
+		log.info("Test cases are executed");
+		assertEquals(true, true);
+
 	}
 
 }
